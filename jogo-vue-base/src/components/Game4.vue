00de6d566f3e3581b431672d361f7670c56b0919
queiroz <ferqueiroz2205@gmail.com>
2025-06-04 21:59:00 -0300

    Alterado as respostas das perguntas e o nome da pergunta
    Alterei as respostas das perguntas e o nome da pergunta que ficou errado

--- jogo-vue-base/src/components/Game4.vue
+++ jogo-vue-base/src/components/Game4.vue
@@ -22,9 +22,9 @@ const bossVidaInicial = 7;
 
 // As perguntas e moedas são definidas diretamente no componente Game4
 const perguntas = {
-  bronze: { resposta: "9", imagem: "/fase4/imgPerguntaBronze.png" },
-  prata: { resposta: "10", imagem: "/fase4/perguntaPrata.png" },
-  dourada: { resposta: "w", imagem: "/fase4/perguntaDourada.png" },
+  bronze:  { resposta: "h", imagem: "/fase4/imgPerguntaBronze.png" },
+  prata:   { resposta: "g", imagem: "/fase4/perguntaPrata.png" },
+  dourada: { resposta: "p", imagem: "/fase4/perguntaDourada.png" },
 };
 
 const moedas = {
@@ -32,16 +32,19 @@ const moedas = {
     "/fase4/moedaBronze1.png",
     "/fase4/moedaBronze2.png",
     "/fase4/moedaBronze3.png",
+    "/fase4/moedaBronze4.png",
   ],
   prata: [
     "/fase4/moedaPrata1.png",
     "/fase4/moedaPrata2.png",
     "/fase4/moedaPrata3.png",
+    "/fase4/moedaPrata4.png",
   ],
   dourada: [
     "/fase4/moedaDourada1.png",
     "/fase4/moedaDourada2.png",
     "/fase4/moedaDourada3.png",
+    "/fase4/moedaDourada4.png",
   ],
 };
 
